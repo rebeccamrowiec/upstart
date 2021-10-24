a39a011f73f45145f24a47d3310a5e556f90efbe
Rebecca Mrowiec <rebecca@bluestag.co.uk>
2021-10-24 18:01:31 +0100

    Adjust logo footer

--- partials/common/logo-footer.php
+++ partials/common/logo-footer.php
@@ -2,9 +2,17 @@
   <div class="container-fluid block-statement__container">
     <div class="row block-statement__row justify-content-center">
       <div class="col-12 col-lg-10 site-footer__col site-footer__col--logos">
-        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/webbox.svg" />
-        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/natwest.png" />
-        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/darwin.png" /> 
+        <div class="site-footer__featured-logo-wrapper">
+          <img class="site-footer__featured-logo site-footer__featured-logo--one" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/webbox.svg" />
+        </div>
+
+        <div class="site-footer__featured-logo-wrapper">
+          <img class="site-footer__featured-logo site-footer__featured-logo--two" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/natwest.png" />
+        </div>
+        
+        <div class="site-footer__featured-logo-wrapper">
+          <img class="site-footer__featured-logo site-footer__featured-logo--three" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/darwin.png" /> 
+        </div>
       </div>
     </div>
   </div>
