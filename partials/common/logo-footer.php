7a3e222ab70a8a0f66e4fa71af5cf2afb31516ac
Rebecca Mrowiec <rebecca@bluestag.co.uk>
2021-10-24 17:10:05 +0100

    Footer

---
+++ partials/common/logo-footer.php
@@ -0,0 +1,11 @@
+<section class="section section--footer site-footer__section site-footer__section--logos">
+  <div class="container-fluid block-statement__container">
+    <div class="row block-statement__row justify-content-center">
+      <div class="col-12 col-lg-10 site-footer__col site-footer__col--logos">
+        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/webbox.svg" />
+        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/natwest.png" />
+        <img class="footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/darwin.png" /> 
+      </div>
+    </div>
+  </div>
+</section>
---
+++ partials/common/main-footer.php
@@ -0,0 +1,52 @@
+<section class="section section--footer site-footer__section site-footer__section--main">
+  <div class="container-fluid site-footer__container">
+    <div class="row site-footer__row site-footer__row--menu justify-content-center">
+      <div class="col-12 col-lg-6 site-footer__col">
+        <img class="site-footer__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/logo.svg" alt="" />
+        <p class="site-footer__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        
+        <?php get_template_part('partials/common/socials'); ?>
+      </div>
+
+      <div class="col-12 col-lg-6 site-footer__col">
+        <h5 class="site-footer__menu-title">Company</h5>
+
+        <div class="site-footer__menu-container">
+          <?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'footer-menu',
+              'container'		 	 => '',
+              'menu_class'     => 'site-footer__menu-wrapper'
+            )
+          );
+          ?>
+
+          <?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'legal-menu',
+              'container'		 	 => '',
+              'menu_class'     => 'site-footer__menu-wrapper'
+            )
+          );
+          ?>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="row site-footer__row justify-content-center">
+      <div class="col-12 col-lg-6 site-footer__col">
+        <span>© Lorem ipsum dolor sit amet</span>
+      </div>
+
+      <div class="col-12 col-lg-6 site-footer__col">
+        <span class="site-footer__item">Lorem ipsum dolor</span>
+        <span class="site-footer__item">Lorem ipsum dolor</span>
+        <span class="site-footer__item">Lorem ipsum dolor</span>
+      </div>
+    </div>
+  </div>
+</section>
